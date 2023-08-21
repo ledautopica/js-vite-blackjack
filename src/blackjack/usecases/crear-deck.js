42d844f69d4124b53a21c705ceaf8154951227fd
ledautopica <vane.multimedia@gmail.com>
2023-08-21 15:58:41 -0600

    Firt commit

---
+++ src/blackjack/usecases/crear-deck.js
@@ -0,0 +1,40 @@
+import _ from 'underscore';
+
+//export const miNombre = 'Vanessa';
+
+// Esta función crea un nuevo deck
+
+/**
+ * Está función crea un nuevo deck
+ * @param {Array<String>} tiposDeCarta Ejemplo: ['C','D','H','S']
+ * @param {Array<String>} tiposEspeciales Ejemplo: ['A','J','Q','K']
+ * @returns {Array<String>} Retorna un nuevo deck de cartas
+ */
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    let deck = [];
+
+    if (!tiposDeCarta || tiposDeCarta.legth ===0 )
+    throw new Error ('tiposDeCarta es obligatorio como un arreglo de string');
+
+    if (!tiposEspeciales || tiposEspeciales.legth ===0 )
+    throw new Error ('tiposEspeciales es obligatorio como un arreglo de string');
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tiposDeCarta ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tiposDeCarta ) {
+        for( let esp of tiposEspeciales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    return deck;
+}
+
+//export default crearDeck;
+
